c99a883e958932276054f347e58e2eff202ca247
Joe Mason <joemas0n@hotmail.co.uk>
2016-12-06 16:06:18 +0000

    fixed the validation

--- dictionaries/dictionaries/Program.cs
+++ dictionaries/dictionaries/Program.cs
@@ -10,11 +10,11 @@ namespace dictionaries
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> codingLanguage = new Dictionary<string, int>();
-            codingLanguage.Add("Java", 1);
-            codingLanguage.Add("Javascript", 2);
-            codingLanguage.Add("C#", 3);
-            codingLanguage.Add("Python", 4);
+            Dictionary<string, string> codingLanguage = new Dictionary<string, string>();
+            codingLanguage.Add("Java", "1");
+            codingLanguage.Add("Javascript", "2");
+            codingLanguage.Add("C#", "3");
+            codingLanguage.Add("Python", "4");
 
             //if(codingLanguage.ContainsKey("Java"))
             //{
@@ -31,8 +31,9 @@ namespace dictionaries
             Console.WriteLine("The amount of value key values with pair is " + count1);
             string result;
             string key1;
-            int value1;
+            string value1;
             int valid = 0;
+ 
             do
             {
                 Console.WriteLine("Press a to add an entry, R to remove an entry, L for Dictornary List, X to Exit ");            
@@ -43,14 +44,22 @@ namespace dictionaries
                         Console.WriteLine("Please enter a key you would like to add");
                         key1 = Console.ReadLine();
                         Console.WriteLine("Please enter a value you would like to add must be integer");
-                        value1 = Convert.ToInt32(Console.ReadLine());
+                        value1 = Console.ReadLine();
                         
-                       
+
+                        if (int.TryParse(value1, out valid))
+                        {
+                            codingLanguage.Add(key1, value1);
+                            Console.WriteLine("working");
+                        } else
+                        {
+                            Console.WriteLine("MUST BE INT");
+                        }
                         break;
                     case "r":
                         Console.WriteLine("Please enter a key, so you can remove a dictornary entry");
                         key1 = Console.ReadLine();
-                        
+                       
 
                         if(codingLanguage.Remove(key1))
                         {
